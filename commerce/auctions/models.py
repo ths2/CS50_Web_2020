cbf6735db526b182c25aad57c63d685df869a3e7
Thomás Silva <ths2.thomas@gmail.com>
2021-03-24 06:42:22 -0300

    Create DB

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=250)
+    image_url = models.CharField(max_length=500)
+    start_bid = models.FloatField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    
+class Bid(models.Model):
+    value_bid = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    listings = models.ManyToManyField(Listing, blank=True, related_name="categories")
